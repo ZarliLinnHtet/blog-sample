c5a1ade8ae427dbb44843ed986720b1a88736c81
ZarliLinnHtet <zarlelinhtet@gmail.com>
2020-01-19 00:44:43 +0630

    layout

---
+++ src/main/java/com/example/blogsample/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.example.blogsample.repository;
+
+import com.example.blogsample.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,Integer> {
+
+}
